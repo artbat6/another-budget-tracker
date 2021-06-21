a4e9cd375cb3177d39dd6885df8fc4e9da837c58
artbat6 <arthurbatesdev@gmail.com>
2021-06-21 15:09:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || || "mongodb://localhost/budget", 
+{
   useFindAndModify: false,
+  useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
@@ -28,3 +28,4 @@ app.use(apiRoutes);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
